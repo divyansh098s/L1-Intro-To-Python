24abb96d36d6f6b8b75bc7105336f781e97ab47e
divyansh098s <agarwaldivyansh354@gmail.com>
2025-04-13 13:23:32 +0530

    My First Python Programme

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello! Python")
+#This Is My First Python Programme
+print("I Study In Sai International School")
+#Performing And Printing A Simple Calculation 
+print("I Love To Play Cricket")
+print("The Result Of 3 + 5 Is : " , 3+5)
